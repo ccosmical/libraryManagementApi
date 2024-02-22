02412e14c5e9c4b4f38e248c0ad3e141e1679b66
ccosmical <101658398+ccosmical@users.noreply.github.com>
2024-02-22 13:59:09 +0300

    other comp

--- librarymanagementapi/librarymanagement/views.py
+++ librarymanagementapi/librarymanagement/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import library_management
+from .serializer import library_managementSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+def library_list(request):
+
+    if request == "GET":
